77212efc8137150ae64520053541c969e2b3ef71
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-07 16:49:20 +0900

    weather information on View

--- WeatherA/WeatherA/ViewController.swift
+++ WeatherA/WeatherA/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
     
+    @IBOutlet weak var weatherStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,14 +29,27 @@ class ViewController: UIViewController {
         }
     }
     
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))℃"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))℃"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))℃"
+    }
+    
     func getCurrentWeather(cityName: String) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=c68563d3baee514daeaf4a57cbef2a19") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
-            debugPrint(weatherInformation)
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+            DispatchQueue.main.async {
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
         }.resume()
     }
 }
