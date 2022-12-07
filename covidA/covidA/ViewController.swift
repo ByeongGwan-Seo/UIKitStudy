ddacfdb10a9a87ebe13d94b5b174a38c27dd8118
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-07 18:10:09 +0900

    초기 생성

---
+++ covidA/covidA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covidA
+//
+//  Created by seobyeonggwan on 2022/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
