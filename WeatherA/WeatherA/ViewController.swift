faf99d4fd2f4f94a182c38b0463b840b543b9976
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-07 16:36:26 +0900

    Fetching weather data

--- WeatherA/WeatherA/ViewController.swift
+++ WeatherA/WeatherA/ViewController.swift
@@ -22,7 +22,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
-        
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=c68563d3baee514daeaf4a57cbef2a19") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
     }
 }
 
--- WeatherA/WeatherA/WeatherInformation.swift
+++ WeatherA/WeatherA/WeatherInformation.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct WeatherInformation: Codable {
     let weather: [Weather]
-    let temp: [Temp]
+    let temp: Temp
     let name: String
     
     enum CodingKeys: String, CodingKey {
@@ -32,7 +32,6 @@ struct Temp: Codable {
     let minTemp: Double
     let maxTemp: Double
         
-    
     enum CodingKeys: String, CodingKey {
         case temp
         case feelsLike = "feels_like"
