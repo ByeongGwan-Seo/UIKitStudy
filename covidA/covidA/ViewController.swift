fd6deffe23bf12d3a14546469611acf1a00ee8c5
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-08 12:59:26 +0900

    fetching data from api

--- covidA/covidA/ViewController.swift
+++ covidA/covidA/ViewController.swift
@@ -6,24 +6,55 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var totalCaseLabel: UILabel!
-    
-    
     @IBOutlet weak var newCaseLabel: UILabel!
-    
-    
     @IBOutlet weak var pieChartView: PieChartView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: {[weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success: \(result)")
+                
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+            
+            
+            
+        })
     }
 
-
+    func fetchCovidOverview(
+      completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+      let url = "https://api.corona-19.kr/korea/country/new/"
+      let param = [
+        "serviceKey": "HVaiR943uEKU1zewm2lWofqDLrx8MhFkQ"
+      ]
+      AF.request(url, method: .get, parameters: param)
+        .responseData(completionHandler: { response in
+          switch response.result {
+          case let .success(data):
+            do {
+              let decoder = JSONDecoder()
+              let result = try decoder.decode(CityCovidOverview.self, from: data)
+              completionHandler(.success(result))
+            } catch {
+              completionHandler(.failure(error))
+            }
+          case let .failure(error):
+            completionHandler(.failure(error))
+          }
+        })
+    }
 }
 
