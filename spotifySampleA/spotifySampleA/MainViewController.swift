25e08183b483160340913acecd35a095330cf296
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-09 14:59:24 +0900

    creating main ui

---
+++ spotifySampleA/spotifySampleA/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  spotifySampleA
+//
+//  Created by seobyeonggwan on 2022/12/09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+}
