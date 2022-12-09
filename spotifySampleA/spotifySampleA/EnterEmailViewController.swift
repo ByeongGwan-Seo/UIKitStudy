eb48ce28e4f07346f20218ff90350a9c7ad717bd
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-09 17:59:59 +0900

    email login/logout with Firebase Auth

--- spotifySampleA/spotifySampleA/EnterEmailViewController.swift
+++ spotifySampleA/spotifySampleA/EnterEmailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController: UIViewController {
     
@@ -19,16 +20,13 @@ class EnterEmailViewController: UIViewController {
         
         nextButton.layer.cornerRadius = 30
         nextButton.isEnabled = false
-        
         emailTextField.delegate = self
         passwordTextField.delegate = self
-        
         emailTextField.becomeFirstResponder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         //navigation bar 보이기
         navigationController?.navigationBar.isHidden = false
         
@@ -37,8 +35,45 @@ class EnterEmailViewController: UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //firebase 이메일/비밀번호 인증
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        //신규 사용자 생성
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self = self else { return }
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: //account already exist
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            } else {
+                self.showMainViewController()
+            }
+        }
     }
     
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
+    }
 }
 
 extension EnterEmailViewController: UITextFieldDelegate {
@@ -48,9 +83,9 @@ extension EnterEmailViewController: UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        let isEmailEmpty = emailTextField.text == ""
-        let isPasswordEmpty = passwordTextField.text == ""
+        let isEmailEmpty = self.emailTextField.text == ""
+        let isPasswordEmpty = self.passwordTextField.text == ""
         
-        nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
+        self.nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
     }
 }
--- spotifySampleA/spotifySampleA/MainViewController.swift
+++ spotifySampleA/spotifySampleA/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     
@@ -19,12 +20,24 @@ class MainViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "customer"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+"""
     }
     
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            debugPrint("Error: signout \(signOutError.description)")
+        }   
     }
-    
-    
 }
