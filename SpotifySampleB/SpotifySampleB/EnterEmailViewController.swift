07ec997ad5b43d44e06a9a1268276317c6c55363
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-12-12 15:59:48 +0900

    basic ui recreated

---
+++ SpotifySampleB/SpotifySampleB/EnterEmailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  EnterEmailViewController.swift
+//  SpotifySampleB
+//
+//  Created by seobyeonggwan on 2022/12/12.
+//
+
+import UIKit
+
+class EnterEmailViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nextButton.layer.cornerRadius = 30
+        
+        nextButton.isEnabled = false
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        emailTextField.becomeFirstResponder()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //navigation bar
+        navigationController?.navigationBar.isHidden = false
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+    }
+    
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+}
+
+extension EnterEmailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let isEmailEmpty = emailTextField.text == ""
+        let isPasswordEmpty = passwordTextField.text == ""
+        
+        nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
+    }
+}
